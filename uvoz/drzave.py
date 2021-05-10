3d8117d67dd2e3266209d171f1f0885d38a6217c
Andraz1999 <49908571+Andraz1999@users.noreply.github.com>
2021-05-10 19:20:04 +0200

    dodal csv, ki se ni to to

--- uvoz/drzave.py
+++ uvoz/drzave.py
@@ -51,4 +51,5 @@ def uvozi_podatke():
 conn = psycopg2.connect(database=auth.db, host=auth.host, user=auth.user, password=auth.password)
 cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor) 
 #############################
-uvozi_podatke()
+#ustvari_tabelo()
+#uvozi_podatke()
---
+++ uvoz/uvozi.py
@@ -0,0 +1,23 @@
+# ### vrstni red, ki bi bil potreben
+
+
+# #### ustvarimo tabele
+
+# drzave.ustvari_tabelo()
+# atrakcije.ustvari_tabelo()
+# atrakcije_po_drzavah.ustvari_tabelo()
+# prevoz.ustvari_tabelo()
+# mozni_transporti.ustvari_tabelo()
+# osebe.ustvari_tabelo()
+# izlet.ustvari_tabelo()
+
+# #### uvozimo podatke
+
+# drzave.uvozi_podatke()
+# atrakcije.uvozi_podatke()
+# atrakcije_po_drzavah.uvozi_podatke()
+# prevoz.uvozi_podatke()
+# mozni_transporti.uvozi_podatke()
+# osebe.uvozi_podatke()
+# izlet.uvozi_tabelo()
+
